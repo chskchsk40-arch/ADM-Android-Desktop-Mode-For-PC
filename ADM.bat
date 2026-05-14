38e6994cb545a6445463231150f85753df1d81c6
chskchsk40-arch <chskchsk40@gmail.com>
2026-05-14 12:55:21 +1000

    Add files via upload

---
+++ ADM.bat
@@ -0,0 +1,15 @@
+@echo off
+title ADM (Android Desktop Mode / DeX)
+
+echo Make sure you Android 13 or higher and you turned on Desktop Mode!!!
+
+start scrcpy --new-display=1920x1080 -K -M --max-fps=170 --video-codec=h265 --video-bit-rate=32M --window-title "ADM (Android Desktop Mode / DeX)"
+
+echo Your ADM is started! If it didn't worked try again.
+echo Instructions:
+echo alt+H = Home button
+echo alt+B/Esc = Back button
+echo alt+S = Resents button
+echo alt+P = Power button
+echo alt+F = Fullscreen
+pause
