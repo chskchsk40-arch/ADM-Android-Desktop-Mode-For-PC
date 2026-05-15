e9a5da3bb149eb5ff135ebac2cae709971d08b87
Cuk <chskchsk40@gmail.com>
2026-05-15 13:34:39 +1000

    ADM.bat

--- ADM.bat
+++ ADM.bat
@@ -5,6 +5,8 @@ echo Make sure you Android 13 or higher and you turned on Desktop Mode!!!
 
 start scrcpy --new-display=1920x1080 -K -M --max-fps=170 --video-codec=h265 --video-bit-rate=32M --window-title "ADM (Android Desktop Mode / DeX)"
 
+adb shell wm density 160
+
 echo Your ADM is started! If it didn't worked try again.
 echo Instructions:
 echo alt+H = Home button
