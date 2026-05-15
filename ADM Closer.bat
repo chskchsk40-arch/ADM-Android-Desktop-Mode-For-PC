dda91ad4d2a44e196a4ec76688537835406c3cc4
Cuk <chskchsk40@gmail.com>
2026-05-15 14:32:53 +1000

    ADM Closer.bat

--- ADM Closer.bat
+++ ADM Closer.bat
@@ -3,4 +3,9 @@ title ADM Closer
 
 adb shell wm density reset
 
-adb shell settings puy global overlay_display_devices ""
+adb shell settings put global overlay_display_devices null
+
+adb shell am force-stop com.android.systemui
+
+echo Try to restart phone if this window didn't close.
+pause
